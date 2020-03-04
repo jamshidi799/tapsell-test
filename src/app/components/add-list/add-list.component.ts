4013ee4d7182952c4fcdc0d05fc244448f4d4a6b
jamshidi799 <jamshidi.m799@gmail.com>
2020-03-04 14:01:33 -0500

    set style of home page

--- src/app/components/add-list/add-list.component.ts
+++ src/app/components/add-list/add-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-list',
-  templateUrl: './add-list.component.html',
-  styleUrls: ['./add-list.component.css']
+  selector: "app-add-list",
+  templateUrl: "./add-list.component.html",
+  styleUrls: ["./add-list.component.css"]
 })
 export class AddListComponent implements OnInit {
+  title: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit() {}
 }
